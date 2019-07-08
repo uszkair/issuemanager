beafead013c18dc4acb0fa6184e5603c17f864f7
uszkair <uszkaimail@gmail.com>
2019-07-09 00:14:24 +0200

    init

--- src/main/java/com/jira/plugins/rest/IssueRestResource.java
+++ src/main/java/com/jira/plugins/rest/IssueRestResource.java
@@ -1,10 +1,18 @@
 package com.jira.plugins.rest;
+//
+//import com.atlassian.jira.component.ComponentAccessor;
+//import com.atlassian.jira.project.Project;
+//import com.atlassian.jira.issue.issuetype.IssueType;
 
+import com.atlassian.jira.component.ComponentAccessor;
+import com.atlassian.jira.project.Project;
 import com.atlassian.plugins.rest.common.security.AnonymousAllowed;
+import sun.awt.AWTAccessor;
 
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Collection;
 
 /**
  * create an issue
@@ -12,13 +20,40 @@ import javax.ws.rs.core.Response;
 @Path("/issue/create")
 public class IssueRestResource {
 
+    private String issueTypeName = null;
+
     @GET
     @AnonymousAllowed
     @Produces({MediaType.APPLICATION_JSON})
     public Response getMessage()
     {
+
+        Project project = ComponentAccessor.getProjectManager().getProjectObjByKey("");
+//        IssueType issueTypeObj = getIssueType(project);
+//
+//
+
+
+
+
+
+
+
+
        IssueRestResourceModel issueRestResourceModel = new IssueRestResourceModel();
-       issueRestResourceModel.setMessage("MAJOM");
+       issueRestResourceModel.setMessage("Hello World");
         return Response.ok(issueRestResourceModel).build();
     }
+
+//    private IssueType getIssueType(Project project){
+//        if(project!=null){
+//            Collection<IssueType> issueTypes = project.getIssueTypes();
+//            for(IssueType issueType: issueTypes){
+//                if(issueType.getName().equals(issueTypeName)){
+//                    return issueType;
+//                }
+//            }
+//        }
+//        return null;
+//    }
 }
